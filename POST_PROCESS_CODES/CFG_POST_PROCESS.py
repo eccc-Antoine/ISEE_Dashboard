626ba58ac660ed05a8aaafec26fee7373dbb99bf
Antoine Maranda <antoine.maranda@ec.gc.ca>
2023-10-17 14:28:46 -0400

    raw_data

--- POST_PROCESS_CODES/CFG_POST_PROCESS.py
+++ POST_PROCESS_CODES/CFG_POST_PROCESS.py
@@ -8,7 +8,7 @@ sections=['USL_CAN', 'LKO_CAN']
 
 years=list(range(1926, 2017))
 
-ISEE_RES=fr'F:\DEM_GLAMM\Dashboard\ISEE_RESULTS'
+ISEE_RES=fr'M:\ISEE_Dashboard\ISEE_RAW_DATA'
 
 POST_PROCESS_RES=fr'M:\ISEE_Dashboard\ISEE_POST_PROCESS'
 
